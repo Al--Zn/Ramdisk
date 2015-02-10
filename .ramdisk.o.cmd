e87ff4ad39c472f55fb264c35aa7df9b8af5b1bd
Al--Zn <alznpku@gmail.com>
2015-02-10 04:35:38 -0800

    Remove redundant files

--- .ramdisk.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lx/Downloads/Ramdisk/ramdisk.o := ld -m elf_x86_64   -r -o /home/lx/Downloads/Ramdisk/ramdisk.o /home/lx/Downloads/Ramdisk/ramdisk_fs.o /home/lx/Downloads/Ramdisk/ramdisk_module.o 
